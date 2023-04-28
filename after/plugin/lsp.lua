9dd987df01331702b02863490bbbee9bbd13707e
Jonas Schenk <jonas.schenk@yahoo.de>
2023-04-28 13:56:22 +0200

    edited stuff

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -17,6 +17,7 @@ lsp.setup_nvim_cmp({
 
 lsp.on_attach(function(client, bufnr)
     local opts = {buffer = bufnr, remap = false}
+    lsp.default_keymaps({buffer = bufnr}
 
     vim.keymap.set('n', "gd", function () vim.lsp.buf.definition() end, opts)
     vim.keymap.set('n', "K", function () vim.lsp.buf.hover() end, opts)
