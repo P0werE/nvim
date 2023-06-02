bea909b0db6576f876aeaadaa8f143de23d4e420
Jonas Schenk <jonas.schenk@yahoo.de>
2023-06-02 13:00:09 +0200

    hidden files are readable

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,23 @@
 local builtin = require('telescope.builtin')
+require('telescope').setup {
+    defaults = {
+        file_ignore_patterns = {
+            "node_modules/",
+            "^.git/",
+        },
+    },
+    pickers = {
+        find_files = {
+            hidden = true,
+        }
+    },
+    extensions = {
+    }
+}
+
+
 vim.keymap.set("n", "<C-p>", builtin.git_files, {})
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<leader>ps', function() 
-	builtin.grep_string({ search = vim.fn.input("Grep > ") })
+vim.keymap.set('n', '<leader>ps', function()
+    builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
